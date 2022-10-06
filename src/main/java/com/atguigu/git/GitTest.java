66025e5b2109a4d857b5d24e7c41e56230548b01
he <he@atguigu.com>
2022-10-06 22:56:01 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author hejianhua
+ * @create 2022-10-06 22:48
+ */
+public class GitTest {
+    public static void main(String[] args){
+        System.out.println("hello git");
+    }
+}
